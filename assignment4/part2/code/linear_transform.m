e1088bfd54eea3e9c9a830bd0b8c286e14d24260
Alexandros Angelakis <aggelakisal@gmail.com>
2022-12-15 16:01:56 +0200

    changed files

--- assignment4/part2/code/linear_transform.m
+++ assignment4/part2/code/linear_transform.m
@@ -1,4 +1,4 @@
 function Z = linear_transform(X,Y)
-    Z = max(max(X)) .* ((Y - min(min(Y))) / max(max(Y)) - min(min(Y))) + min(min(X));
+    Z = max(max(X)) .* ((Y - min(min(Y))) ./ (max(max(Y)) - min(min(Y)))) + min(min(X));
 end
 
